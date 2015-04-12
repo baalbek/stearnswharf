d3cc64a6325e7aea9696662a7ad91ea6c969ef9c
rcs <rolf.c.stadheim@gmail.com>
2015-04-12 23:01:01 +0200

    Nodes selected based on use in system

--- src/StearnsWharf/Repos/NodeRepository.hs
+++ src/StearnsWharf/Repos/NodeRepository.hs
@@ -1,16 +1,19 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE OverloadedStrings #-}
 
 
 module StearnsWharf.Repos.NodeRepository where
 
+import Text.Printf (printf)
+import qualified Data.ByteString.UTF8 as UTF8 
 import qualified Data.Map as Map
 import Control.Monad.State (State,runState,get,put)
 import Control.Monad (liftM3)
 import Control.Applicative ((<$>),(<*>))
 import Database.PostgreSQL.Simple (Connection,query)
+import Database.PostgreSQL.Simple.Types (Query(..))
 import Database.PostgreSQL.Simple.FromRow (FromRow,fromRow,field)
 
 import StearnsWharf.Common (getConnection)
@@ -22,11 +25,24 @@ type NodeDef = (Int,N.Node)
 instance FromRow N.Node where
     fromRow = N.Node <$> field <*> field <*> field <*> field <*> liftM3 N.Dof field field field <*> field
 
+sql :: Query
+sql = Query (UTF8.fromString (printf "%s %s %s %s" s1 s2 s3 s4 :: String))
+    where s1 = "select n.oid,n.x,n.y,n.z,n.dofx,n.dofz,n.dofm,0 from construction.nodes n"
+          s2 = "where n.oid in (select n1 from construction.steel_elements where sys_id=?)"
+          s3 = "or n.oid in (select n2 from construction.steel_elements where sys_id=?)"
+          s4 = "order by n.x,n.y"
+
+{-
+    where s1 = "select n.oid,n.x,n.y,n.z,n.dofx,n.dofz,n.dofm,0 from construction.nodes n"
+          s2 = "join construction.systems s on s.project_id=n.project_id and s.coord_sys=n.coord_sys"
+          s3 = "where s.oid=? order by n.x,n.y " 
+-}
+
 fetchNodes :: Connection 
               -> Int           -- ^ System Id 
               -> IO [N.Node]
 fetchNodes conn sysId = 
-    (query conn "select n.oid,n.x,n.y,n.z,n.dofx,n.dofz,n.dofm,0 from construction.nodes n join construction.systems s on s.project_id=n.project_id and s.coord_sys=n.coord_sys where s.oid=?" [sysId]) :: IO [N.Node]
+    (query conn sql [sysId,sysId]) :: IO [N.Node]
 
 matstatNodeDef :: N.Node -> State Int NodeDef
 matstatNodeDef node = 
